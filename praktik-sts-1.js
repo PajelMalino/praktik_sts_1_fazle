558408944ffb880305ebe3e2ac6bbafe1c8911e8
Fazle <muhammadpajelmalino@gmail>
2024-10-07 08:44:05 +0700

    first commit

--- praktik-sts-1.js
+++ praktik-sts-1.js
@@ -1,19 +1,39 @@
 function validateName(name) {
-    if (name === "") {
-        return "Nama tidak Valid"
-    }
+  if (name === "") {
+    return "Nama tidak valid";
+  }
 
-    if (name.length < 3) {
-        return "Nama terlalu pendek"
-    }
+  if (name.length < 3) {
+    return "Nama terlalu pendek";
+  }
+
+  return "Nama Valid";
 }
 
 function validateAddress(alamat) {
-    if (alamat === "") {
-        return "Alamat tidak Valid"
+  if (alamat === "") {
+    return "Alamat tidak valid";
+  }
+
+  if (address.length < 10) {
+    return "Alamat terlalu pendek";
+  }
+
+  return "Alamat valid";
+}
+
+function validatePhoneNumber(nomortelepon) {
+    if (nomortelepon === "") {
+        return "Nomor telepon tidak Valid"
     }
 
-    if (alamat.length < 10) {
-        return "Alamat terlalu pendek"
+    if (nomortelepon.length < 10 || nomortelepon.length > 13) {
+        return "Panjang nomor telepon tidak valid"
     }
+
+    if (nomortelepon.test(nomortelepon)) {
+        return "Nomor telepon hanya boleh berisi angka"
+    }
+
+    return "Nomor telepon Valid"
 }
